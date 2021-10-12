fb110eff75c78e9d31845ad11be00017875007db
miunmnn <esteban.principe@utec.edu.pe>
2021-10-12 00:08:57 -0500

    Added pseudocode

--- src/extendible_hash/extendible_hash.cpp
+++ src/extendible_hash/extendible_hash.cpp
@@ -3,13 +3,33 @@
 
 using namespace std;
 
-
-extendible_hash::extendible_hash(const filesystem::path& index_path):index_file(index_path,
-		ios::binary |
-		ios::in |
-		ios::out
-	)
-{
-
+extendible_hash::extendible_hash(int D, int fb, string index_filename){
+    this->D = D;
+    this->fb = fb;
+    this->index_file.open(index_filename, ios::in | ios::binary);
+	//index_file.read((char*)&h, sizeof(h));
 }
 
+extendible_hash::~extendible_hash(){
+    this->index_file.close();
+}
+
+
+bool extendible_hash::insert(Registro registro){
+    /* ver si index esta vacio
+        - si esta vacio:
+            Crear 0 y 1
+        Hallar valor H de la key del registro
+        Valor H a hash -> binario
+        Buscar en el indice LSB, HI
+        Buscar el bucket -> ver si esta lleno, si no es asi, agregar registro al Bucket. Si es asi, hacer el resize.
+        Resize: 
+            si d < D
+                Agregar bit a la izquierda de HI(agregar 0 y 1)
+                Cambiar HI por 0+HI
+                Escribir en el indice el 1+HI
+                Crear nodo en el data File asociado a 1+HI
+                Volver a insertar los registros que estaban en el bucket y el nuevo registro
+                Actualizar el contador de los nodos -> actualizar cantidad_de_nodos_en_bucket
+    */
+}
--- src/extendible_hash/extendible_hash.hpp
+++ src/extendible_hash/extendible_hash.hpp
@@ -15,12 +15,15 @@ using namespace std;
 
 class extendible_hash{
     private:
-        struct header{
-            //Global Depth
-            int D;
-            // Max number of key-position pairs on a bucket
-            int bucket_max_elements;
-        };
+        // struct header{
+        //     //Global Depth
+        //     int D;
+        //     // Max number of key-position pairs on a bucket
+        //     int bucket_max_elements;
+        // };
+        
+        int D;
+        int fb; 
         struct bucket_header{
             /// The local depth.
             int d;
@@ -36,61 +39,59 @@ class extendible_hash{
             /// The position of the key in the data file.
             int position;
         };
-        header h;
+
+        struct bucket{
+            
+        };
+
+        //header h;
 	    fstream index_file;
-
-        void write_empty_bucket(int d);
-
-        /// Get the size of bucket_header, it's pairs and the pointer.
-        int bucket_size() const;
-
-        /// Get the d least significant bits from the hash.
-        int d_bit(int d, int key_hash) const;
-
-        /// Get the D least significant bits from the hash.
-        int D_bit(int key_hash) const;
-
-        /// Returns 0(d_bit(d,key)) 1(d_bit(d,key))
-        pair<int, int> get_new_d_bits(int d, int key_hash) const;
-
-        /// Get the position of the directory of a hash.
-        int get_directory(int key_hash) const;
-
-        /// Get the position of the first bucket.
-        int get_first_bucket() const;
-
-        /// Get the pointer of a bucket from a hash.
-        int get_bucket(int key_hash);
-
-        /// Updates the header.
-        void set_bucket_header(int pos, bucket_header h);
-
-        /// Updates the header's pointer
-        void set_pointer(int pos, int b_p);
-
-        /// Gets a header from a bucket.
-        bucket_header get_bucket_header(int pos);
-
-        /// Gets a pointer from a bucket.
-        int get_pointer(int pos);
-
-        /// Add a pair to a bucket with enough space.
-        void add_pair(int pos, key_position kp);
-
+        
     public:
-    	/// Use existing index.
-        extendible_hash(const filesystem::path& index_path);
-
         /// Create a new index.
         extendible_hash(
-            const filesystem::path& index_path,
             int D,
-            int bucket_max_elements
+            int fb,
+            string index_filename
         );
 
+    struct Registro {
+        char codigo [6];
+        char nombre [21];
+        char carrera [16];
+        int ciclo;
+        int next = 0;
+
+        void setData() {
+            cout << "Codigo:\n";
+            cin >> codigo;
+            cout << "Nombre:\n";
+            cin >> nombre;
+            cout << "Carrera:\n";
+            cin >> carrera;
+            cout << "Ciclo:\n";
+            cin >> ciclo;
+            cout << "Next:\n";
+            cin >> next;
+        }
+
+        void show() {
+            cout << '\n';
+            cout << codigo << '\n';
+            cout << nombre << '\n';
+            cout << carrera << '\n';
+            cout << ciclo << '\n';
+            cout << next << '\n';
+            cout << '\n';
+        }
+
+    };
         bool is_open() const;
 
         vector<int> get_positions(int key_hash);
-        bool insert(int key_hash, int position);
+        bool insert(Registro registro);
         bool delete_from(int key_hash, int position);
+
 };
+
+
